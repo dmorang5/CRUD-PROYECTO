9035773830621433db9a6a3d5a85733efed5abbe
dmorang5 <96917040+dmorang5@users.noreply.github.com>
2022-01-27 13:05:34 -0500

    Add files via upload

---
+++ CRUD PROYECTO/cargo.py
@@ -0,0 +1,13 @@
+class Cargo:
+  secuencia=2
+  cargos = [{"codigo":1,"cargo":"Analista"}, {"codigo":2,"cargo":"Jefe"}]
+  def __init__(self, cargo):
+    Cargo.secuencia += 1
+    self.codigo = Cargo.secuencia
+    self.descripcion = cargo
+  def  registro(self):
+    return {"codigo":self.codigo,"cargo":self.descripcion}
+        
+# carg = Cargo("Gerente")
+# Cargo.cargos.append(carg.registro())
+# print(Cargo.cargos)
